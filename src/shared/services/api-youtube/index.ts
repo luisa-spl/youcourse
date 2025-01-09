2bc23535000d93047647ba59bf5d6abcf39d9ba4
luisa spinola <luisa.spinola@cubos.academy>
2025-01-09 16:44:12 -0300

    feat: add getById call

--- src/shared/services/api-youtube/index.ts
+++ src/shared/services/api-youtube/index.ts
@@ -1,4 +1,4 @@
-import { youtube } from "@googleapis/youtube";
+import { youtube, youtube_v3 } from "@googleapis/youtube";
 
 const fetchWithNextConfig = (nextConfig?: NextFetchRequestConfig): typeof fetch => (input, params = {}) => {
   return fetch(input, { ...params, next: nextConfig });
@@ -28,10 +28,86 @@ export const APIYouTube = {
         image: item.snippet?.thumbnails?.medium?.url || '',
       }));
 
-      return courses;
-    }
-  },
-  classes: {
+      return courses.filter(course => course.description.includes('CODARSE'));
+    },
+    getById: async (id: string) => {
+      const { data: { items: [courseItem] = [] }} = await YouTubeAPIClient.playlists.list({
+        id: [id],
+        maxResults: 1,
+        part: ['snippet'],
+      }, {
+        fetchImplementation: fetchWithNextConfig({ revalidate: (60 * 60) * 48 })
+      });
 
-  }
-}
+      const classes: youtube_v3.Schema$PlaylistItem[] = [];
+      let nextPageToken: string | undefined = undefined;
+      do {
+        await YouTubeAPIClient.playlistItems
+        .list({
+          playlistId: id,
+          maxResults: 50,
+          part: ['snippet'],
+          pageToken: nextPageToken,
+        }, { 
+          fetchImplementation: fetchWithNextConfig({ revalidate: (60 * 60) * 24 }) 
+        })
+          .then(({ data }) => {
+            classes.push(...(data.items || []));
+            nextPageToken = data.nextPageToken || undefined;
+          });
+      } while (nextPageToken);
+
+      type TGroupWithClass = {
+        title: string,
+        courseId: string,
+        classes: {
+          id: string,
+          title: string,
+        }[],
+      }
+      const classGroups = classes
+        .sort((a, b) => (a.snippet?.position || 0) - (b.snippet?.position || 0))
+        .map(youTubePlaylistItem => ({
+          id: youTubePlaylistItem.id || '',
+          title: youTubePlaylistItem.snippet?.title || '',
+          description: youTubePlaylistItem.snippet?.description || '',
+        }))
+        .reduce<TGroupWithClass[]>((previous, current) => {
+          const currentGroupTitle = current.description.match(/CODARSE - .*/)?.at(0)?.replace('CODARSE - ', '').trim() || '';
+
+          const previousGroup = previous.at(previous.length - 1);
+          const previousGroupTitle = previousGroup?.title;
+
+          if (previousGroup && previousGroupTitle === currentGroupTitle) {
+            previousGroup.classes.push({
+              id: current.id,
+              title: current.title,
+            });
+          } else {
+            previous.push({
+              courseId: id,
+              title: currentGroupTitle,
+              classes: [
+                {
+                  id: current.id,
+                  title: current.title,
+                }
+              ]
+            });
+          }
+  
+          return previous;
+        }, []);
+
+        return {
+          id,
+          title: courseItem.snippet?.title || '',
+          description: courseItem.snippet?.description || '',
+          image: courseItem.snippet?.thumbnails?.medium?.url || '',
+  
+          classGroups,
+          numberOfClasses: classes.length,
+        };
+    },
+  },
+};
