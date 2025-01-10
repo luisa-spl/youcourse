803e6f9f91a15646e42017ea4f162d38c0e9faac
luisa spinola <luisa.spinola@cubos.academy>
2025-01-10 16:36:51 -0300

    feat: adding font family and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,19 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Heebo, Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ['latin'] });
+const heebo = Heebo({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "YouCourse Platform",
-  description: "Your courses and just it",
+  title: "YouCourse | Sua melhor plataforma de cursos",
+  description: "Seus cursos com a melhor experiência de foco",
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt-Br">
-      <body className={`${inter.className}`}>
+      <body className={`${inter.className} ${heebo.className}`}>
         {children}
       </body>
     </html>
