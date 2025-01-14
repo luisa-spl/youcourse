9c2dade78419d2a6687c1818973f6af824731621
luisa spinola <luisa.spinola@cubos.academy>
2025-01-14 17:26:26 -0300

    chore: starting responsive header

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { MdMenu, MdOutlineSettings } from "react-icons/md";
 
 export function Header() {
   const currentPath = usePathname();
@@ -11,16 +12,19 @@ export function Header() {
     <header className="flex items-center justify-between bg-primary p-4 px-8">
       <Image src="/youcourse.png" alt="youcourse logotipo" width={50} height={50} />
       <nav className="flex gap-8">
-        <ul className="flex items-center gap-8">
+        <ul className="hidden md:flex items-center gap-8">
           <li>
-            <Link href="/" data-isActive={currentPath === '/'} className="data-[isActive=true]:underline">Home</Link>
+            <Link href="/" data-isactive={currentPath === '/'} className="data-[isactive=true]:underline">Home</Link>
           </li>
           <li>
-            <Link href="/cursos" data-isActive={currentPath === '/cursos'} className="data-[isActive=true]:underline">Cursos</Link>
+            <Link href="/cursos" data-isactive={currentPath === '/cursos'} className="data-[isactive=true]:underline">Cursos</Link>
           </li>
         </ul>
-        <button onClick={() => window.document.body.setAttribute('light', 'true')}>
-          <Image src="/settings.png" alt="youcourse logotipo" width={25} height={25} />
+        <button onClick={() => window.document.body.setAttribute('light', 'true')} className="hidden md:flex gap-8">
+          <MdOutlineSettings size="24"/>
+        </button>
+        <button className="flex md:hidden">
+          <MdMenu size="24" />
         </button>
       </nav>
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
         primary: 'var(--color-primary)',
         background: 'var(--color-background)',
         'primary-contrast': 'var(--color-primary-contrast)',
+      },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
       }
     },
   },
