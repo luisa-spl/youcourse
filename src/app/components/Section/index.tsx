5b4788deb39952004c3d0f7497100cb66b12e030
luisa spinola <luisa.spinola@cubos.academy>
2025-02-17 12:25:24 -0300

    feat: adding nav buttons and styling scroll

--- src/app/components/Section/index.tsx
+++ src/app/components/Section/index.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { UIEvent, useRef, useState } from "react";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import { CourseCard } from "../Card";
 
 type CourseItem = {
@@ -14,6 +18,24 @@ interface SectionProps {
 }
 
 export function Section({ title, items, variant = 'grid' }: SectionProps) {
+  const scrollRef = useRef<HTMLUListElement>(null);
+  const [scrollAt, setScrollAt] = useState<'start' | 'middle' | 'end'>('start');
+
+  const handleScroll = (scroll: number): void => {
+    const currentScrollLeft = scrollRef.current?.scrollLeft || 0;
+    scrollRef.current?.scrollTo({ behavior: 'smooth', left: currentScrollLeft + scroll });
+  };
+
+  const handleSetScroll = (event: UIEvent<HTMLUListElement>) => {
+    if (event.currentTarget.scrollLeft === 0) {
+      setScrollAt('start');
+    } else if ((event.currentTarget.scrollWidth - event.currentTarget.clientWidth) === event.currentTarget.scrollLeft) {
+      setScrollAt('end');
+    } else {
+      setScrollAt('middle');
+    }
+  };
+  
   return (
     <section className="flex flex-col gap-4 px-4">
       <h2 className="font-bold text-3xl">
@@ -21,13 +43,35 @@ export function Section({ title, items, variant = 'grid' }: SectionProps) {
       </h2>
 
       <ul 
-        className={`grid grid-cols-1 sm:grid-cols-none ${variant === 'grid' ? 'sm:grid-cols-2 xl:grid-cols-3' : 'sm:grid-flow-col sm:overflow-x-auto'} gap-4 sm:gap-8 max-w-screen-xl`}
+        ref={scrollRef}
+        onScroll={handleSetScroll}
+        className={`overflow-primary grid grid-cols-1 sm:grid-cols-none ${variant === 'grid' ? 'sm:grid-cols-2 xl:grid-cols-3' : 'sm:grid-flow-col sm:overflow-x-auto pb-3'} gap-4 sm:gap-8 max-w-screen-xl`}
       >
+        {variant === 'h-list' && (
+          <button 
+            disabled={scrollAt === 'start'}
+            onClick={() => handleScroll(-350)} 
+            className="h-14 w-14 bg-primary rounded-full flex justify-center items-center sticky my-auto left-0 -ml-20 disabled:opacity-0 transition-opacity"
+          >
+            <MdKeyboardArrowLeft size={32}/>
+          </button>
+        )}
+
         {items.map(item => (
           <li key={item.id} className="w-full sm:w-96">
             <CourseCard imageURL={item.image} title={item.title} description={item.description} id={item.id}  />
           </li>
         ))}
+
+        {variant === 'h-list' && (
+          <button
+            disabled={scrollAt === 'end'}
+            onClick={() => handleScroll(350)} 
+            className="h-14 w-14 bg-primary rounded-full flex justify-center items-center sticky my-auto right-0 -ml-20 disabled:opacity-0 transition-opacity"
+          >
+            <MdKeyboardArrowRight size={32}/>
+          </button>
+        )}
       </ul>
     </section>
   );
